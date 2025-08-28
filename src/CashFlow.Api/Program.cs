de6b1c55a711d8f35e7f335312f636426c3c9c67
Gabriel Souza Campos <gabriel.s.campos@hotmail.com>
2025-08-28 19:07:50 -0300

    feat: add JWT authentication middleware to protect private routes
    - Implemented middleware for JWT token validation
    - Adjusted response for unauthorized access

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -3,7 +3,10 @@ using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +18,37 @@ builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(
+                builder.Configuration.GetValue<string>("Settings:Jwt:SingingKey")!
+            )
+        ),
+    };
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference("/docs");
+    app.MapScalarApiReference("/docs", config =>
+    {
+        config.Title = "CashFlow API Documentation";
+        config.Theme = ScalarTheme.BluePlanet;
+        config.WithDocumentDownloadType(DocumentDownloadType.Json);
+    });
 }
 
 app.UseMiddleware<CultureMiddleware>();
@@ -28,6 +56,7 @@ app.UseMiddleware<CultureMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
